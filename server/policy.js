a8778adc3a3161335018ac44c71fa50048de3370
Kiril Seksenov <seksenovk@gmail.com>
2015-11-02 15:45:51 -0800

    adding CSP

---
+++ server/policy.js
@@ -0,0 +1,5 @@
+BrowserPolicy.content.disallowInlineScripts();
+BrowserPolicy.content.disallowEval();
+BrowserPolicy.content.allowOriginForAll("http://raw.github.com/")
+BrowserPolicy.content.allowOriginForAll("https://raw.github.com/")
+BrowserPolicy.content.allowOriginForAll("https://raw.githubusercontent.com/")
